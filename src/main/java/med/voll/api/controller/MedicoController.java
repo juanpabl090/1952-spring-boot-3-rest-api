48487ee6b2c290be2b35d6d421efaf115a218a03
juanpabl090 <juanrona12@gmail.com>
2024-07-09 19:33:19 -0600

    agregamos funcionalidad de paginacion, agregamos propiedades para ver mas visual las consultas de sql con hibernate y jpa

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -6,9 +6,10 @@ import med.voll.api.medico.DatosRegistroMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -23,9 +24,8 @@ public class MedicoController {
     }
 
     @GetMapping
-    public List<DatosListadoMedico> listadoMedicos() {
-        return medicoRepository.findAll().stream()
-                .map(DatosListadoMedico::new).toList();
+    public Page<DatosListadoMedico> listadoMedicos(@PageableDefault(size = 2) Pageable pageable) {
+        return medicoRepository.findAll(pageable).map(DatosListadoMedico::new);
     }
 
 }
