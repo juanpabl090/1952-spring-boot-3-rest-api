d74acedce217a6b03d75b39c97cabf53cadf126a
juanpabl090 <juanrona12@gmail.com>
2024-07-09 18:21:55 -0600

    first commit

---
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -0,0 +1,31 @@
+package med.voll.api.controller;
+
+import jakarta.validation.Valid;
+import med.voll.api.medico.DatosListadoMedico;
+import med.voll.api.medico.DatosRegistroMedico;
+import med.voll.api.medico.Medico;
+import med.voll.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/medicos")
+public class MedicoController {
+
+    @Autowired
+    private MedicoRepository medicoRepository;
+
+    @PostMapping
+    public void registrarMedico(@RequestBody @Valid DatosRegistroMedico datosRegistroMedico) {
+        medicoRepository.save(new Medico(datosRegistroMedico));
+    }
+
+    @GetMapping
+    public List<DatosListadoMedico> listadoMedicos() {
+        return medicoRepository.findAll().stream()
+                .map(DatosListadoMedico::new).toList();
+    }
+
+}
